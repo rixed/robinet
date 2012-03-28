eca455b42790cf1e068b946e66118eee8e692858
Cedric Cellier <rixed@happyleptic.org>
2012-03-28 22:29:21 +0200

    [Doc] Start to ocamldocument some modules.

--- packet.ml
+++ packet.ml
@@ -17,96 +17,124 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with RobiNet.  If not, see <http://www.gnu.org/licenses/>.
  *)
+(**
+ * The purpose of this module is to help individual packet examination and
+ * manipulation. It is not used when simulating a network.
+ *
+ * This is the module to look for if you want to fiddle with packets on a
+ * one to one basis, for instance to fuzz some packet source, collect some
+ * statistics on individual packets, search for some packets in a pcap file,
+ * etc...
+ *
+ * Due to the fact that every packet is considered in isolation, is can only
+ * decode the simpler protocols for which all required data are within a
+ * single packet. In other words, it will not go much deeper than TCP/UDP.
+ *)
 open Batteries
 open Bitstring
 open Tools
 
-let debug = false
+(** {1 (Un)Packing} *)
 
-(* Pack/Unpack the whole protocol stack *)
-
+(** Pack/Unpack the whole protocol stack that fits entirely within a packet.
+ *
+ * Each raw packet can be seen either as a [bitstring] or as a pile of layers (for
+ * instance, DNS over UDP over IP over Ethernet). This Module will convert between these
+ * two representations, without loss of information.
+ * Typically you want to unpack then update, then pack back to [bitstring]. *)
 module Pdu = struct
-    type layer = Raw of bitstring
-             | Dhcp of Dhcp.Pdu.t | Eth of Eth.Pdu.t | Arp of Arp.Pdu.t
-             | Ip   of Ip.Pdu.t   | Udp of Udp.Pdu.t | Tcp of Tcp.Pdu.t
-             | Dns of Dns.Pdu.t   | Sll of Sll.Pdu.t
-    type t = layer list (* with outer layer first for more natural presentation *)
+    (** Each layer can be of any one of these known protocol. *)
+    type layer = Raw of bitstring (** the fallback when the actual protocol is not known *)
+               | Dhcp of Dhcp.Pdu.t | Eth of Eth.Pdu.t | Arp of Arp.Pdu.t
+               | Ip   of Ip.Pdu.t   | Udp of Udp.Pdu.t | Tcp of Tcp.Pdu.t
+               | Dns of Dns.Pdu.t   | Sll of Sll.Pdu.t
+    (** A Pdu.t is a list of {!Packet.Pdu.layer}s, with the outer layer first for
+     * a more natural presentation when printed. *)
+    type t = layer list
     
-    let new_payload bits = function
-        | Raw _  -> Raw bits
-        (* can you spot a pattern here? *)
-        | Eth  p -> Eth { p with Eth.Pdu.payload = Payload.o bits }
-        | Sll  p -> Sll { p with Sll.Pdu.payload = Payload.o bits }
-        | Ip   p -> Ip  { p with Ip.Pdu.payload = Payload.o bits }
-        | Udp  p -> Udp { p with Udp.Pdu.payload = Payload.o bits }
-        | Tcp  p -> Tcp { p with Tcp.Pdu.payload = Payload.o bits }
-        | x -> x
-
-    let pack_1 = function (* there ought to be a better way *)
-        | Dhcp t -> Dhcp.Pdu.pack t | Eth t -> Eth.Pdu.pack t
-        | Arp t  -> Arp.Pdu.pack t  | Ip t  -> Ip.Pdu.pack t
-        | Udp t  -> Udp.Pdu.pack t  | Tcp t -> Tcp.Pdu.pack t
-        | Dns t  -> Dns.Pdu.pack t  | Sll t -> Sll.Pdu.pack t
-        | Raw t  -> t
-    let pack t =
+    (** [pack pdu] converts the layer list back to a [bitstring]. *)
+    let pack (t:t) =
+        let new_payload bits = function
+            | Raw _  -> Raw bits
+            (* can you spot a pattern here? *)
+            | Eth  p -> Eth { p with Eth.Pdu.payload = Payload.o bits }
+            | Sll  p -> Sll { p with Sll.Pdu.payload = Payload.o bits }
+            | Ip   p -> Ip  { p with Ip.Pdu.payload = Payload.o bits }
+            | Udp  p -> Udp { p with Udp.Pdu.payload = Payload.o bits }
+            | Tcp  p -> Tcp { p with Tcp.Pdu.payload = Payload.o bits }
+            | x -> x in
+        let pack_1 = function (* there ought to be a better way *)
+            | Dhcp t -> Dhcp.Pdu.pack t | Eth t -> Eth.Pdu.pack t
+            | Arp t  -> Arp.Pdu.pack t  | Ip t  -> Ip.Pdu.pack t
+            | Udp t  -> Udp.Pdu.pack t  | Tcp t -> Tcp.Pdu.pack t
+            | Dns t  -> Dns.Pdu.pack t  | Sll t -> Sll.Pdu.pack t
+            | Raw t  -> t in
         let rec aux bits = function
             | [] -> Option.get bits
             | p :: ps ->
                 (* bits, if set, is the new payload for p *)
                 let p' = match bits with None -> p | Some b -> new_payload b p in
-                aux (Some (pack_1 p')) ps in
+                aux (Some (pack_1 p')) ps
+        in
         aux None (List.rev t)
 
-    let unpack_raw bits = if bitstring_is_empty bits then [] else [ Raw bits ]
-    let try_unpack unp do_t bits =
-        match unp bits with None -> unpack_raw bits
-                          | Some x -> do_t x
-    let unpack_dhcp = try_unpack Dhcp.Pdu.unpack (fun x -> [ Dhcp x])
-    let unpack_dns  = try_unpack Dns.Pdu.unpack  (fun x -> [ Dns x])
-    let unpack_arp  = try_unpack Arp.Pdu.unpack  (fun x -> [ Arp x])
-    let unpack_ports src dst = (match src, dst with
-            | 53, _ | _, 53 -> unpack_dns
-            | 67, _ | _, 67 -> unpack_dhcp
-            | _ -> unpack_raw)
-    let unpack_tcp = try_unpack Tcp.Pdu.unpack (fun tcp -> Tcp tcp ::
-                    (if Payload.length tcp.Tcp.Pdu.payload > 0 then
-                        (unpack_ports (tcp.Tcp.Pdu.src_port :> int)
-                                      (tcp.Tcp.Pdu.dst_port :> int)
-                                      (tcp.Tcp.Pdu.payload :> bitstring))
-                    else []))
-    let unpack_udp = try_unpack Udp.Pdu.unpack (fun udp -> Udp udp ::
-                    (unpack_ports (udp.Udp.Pdu.src_port :> int)
-                                  (udp.Udp.Pdu.dst_port :> int)
-                                  (udp.Udp.Pdu.payload :> bitstring)))
-    let unpack_ip  = try_unpack Ip.Pdu.unpack (fun ip -> Ip ip ::
-                ((if ip.Ip.Pdu.proto = Ip.Proto.tcp then unpack_tcp
-                  else if ip.Ip.Pdu.proto = Ip.Proto.udp then unpack_udp
-                  else unpack_raw) (ip.Ip.Pdu.payload :> bitstring)))
-    let unpack_eth = try_unpack Eth.Pdu.unpack (fun eth -> Eth eth ::
-                ((if eth.Eth.Pdu.proto = Arp.HwProto.ip4 then unpack_ip
-                  else if eth.Eth.Pdu.proto = Arp.HwProto.arp then unpack_arp
-                  else unpack_raw) (eth.Eth.Pdu.payload :> bitstring)))
-    let unpack_sll = try_unpack Sll.Pdu.unpack (fun sll -> Sll sll ::
-                ((if sll.Sll.Pdu.proto = Arp.HwProto.ip4 then unpack_ip
-                  else if sll.Sll.Pdu.proto = Arp.HwProto.arp then unpack_arp
-                  else unpack_raw) (sll.Sll.Pdu.payload :> bitstring)))
-
+    (** Convert a [bitstring] into a {!Packet.Pdu.t}.
+     * @param dlt if the {e data link layer} is not Ethernet then you can change it here.
+     *            The only other known {e DLT} is {!Pcap.dlt_linux_cooked}, though. *)
     let unpack ?(dlt=Pcap.dlt_en10mb) bs =
+        let unpack_raw bits =
+            if bitstring_is_empty bits then [] else [ Raw bits ] in
+        let try_unpack unp do_t bits =
+            match unp bits with None -> unpack_raw bits
+                              | Some x -> do_t x in
+        let unpack_dhcp = try_unpack Dhcp.Pdu.unpack (fun x -> [ Dhcp x]) in
+        let unpack_dns  = try_unpack Dns.Pdu.unpack  (fun x -> [ Dns x]) in
+        let unpack_arp  = try_unpack Arp.Pdu.unpack  (fun x -> [ Arp x]) in
+        let unpack_ports src dst = (match src, dst with
+                | 53, _ | _, 53 -> unpack_dns
+                | 67, _ | _, 67 -> unpack_dhcp
+                | _ -> unpack_raw) in
+        let unpack_tcp = try_unpack Tcp.Pdu.unpack (fun tcp -> Tcp tcp ::
+                        (if Payload.length tcp.Tcp.Pdu.payload > 0 then
+                            (unpack_ports (tcp.Tcp.Pdu.src_port :> int)
+                                          (tcp.Tcp.Pdu.dst_port :> int)
+                                          (tcp.Tcp.Pdu.payload :> bitstring))
+                        else [])) in
+        let unpack_udp = try_unpack Udp.Pdu.unpack (fun udp -> Udp udp ::
+                        (unpack_ports (udp.Udp.Pdu.src_port :> int)
+                                      (udp.Udp.Pdu.dst_port :> int)
+                                      (udp.Udp.Pdu.payload :> bitstring))) in
+        let unpack_ip  = try_unpack Ip.Pdu.unpack (fun ip -> Ip ip ::
+                    ((if ip.Ip.Pdu.proto = Ip.Proto.tcp then unpack_tcp
+                      else if ip.Ip.Pdu.proto = Ip.Proto.udp then unpack_udp
+                      else unpack_raw) (ip.Ip.Pdu.payload :> bitstring))) in
+        let unpack_eth = try_unpack Eth.Pdu.unpack (fun eth -> Eth eth ::
+                    ((if eth.Eth.Pdu.proto = Arp.HwProto.ip4 then unpack_ip
+                      else if eth.Eth.Pdu.proto = Arp.HwProto.arp then unpack_arp
+                      else unpack_raw) (eth.Eth.Pdu.payload :> bitstring))) in
+        let unpack_sll = try_unpack Sll.Pdu.unpack (fun sll -> Sll sll ::
+                    ((if sll.Sll.Pdu.proto = Arp.HwProto.ip4 then unpack_ip
+                      else if sll.Sll.Pdu.proto = Arp.HwProto.arp then unpack_arp
+                      else unpack_raw) (sll.Sll.Pdu.payload :> bitstring)))
+        in
         if dlt = Pcap.dlt_linux_cooked then unpack_sll bs
         else unpack_eth bs
 
 end
 
-(* Shorthands *)
+(** {1 Shorthands} *)
 
+(** [Packet.of_pcap pkt] converts a raw packet (as returned by {!Pcap.read_next_pkt}
+ * or {!Pcap.enum_of}) into a printable/editable {!Packet.Pdu.t}. *)
 let of_pcap ?dlt (_ts, bits) =
     Pdu.unpack ?dlt bits
 
+(** [Packet.enum_of filename] reads a pcap file and returns an [Enum.t] of {!Packet.Pdu.t}. *)
 let enum_of fname =
     let dlt = Pcap.dlt_of fname in
     Pcap.enum_of fname /@ of_pcap ~dlt
 
-(* Check that we manage to decode the actual concent of the packets by counting the cnx establishments *)
+(* Check that we manage to decode the actual content of the packets by counting the cnx establishments *)
 (*$= enum_of & ~printer:string_of_int
     (enum_of "tests/someweb.pcap" // \
         (function _ :: _ :: Pdu.Tcp { Tcp.Pdu.flags = { Tcp.Pdu.syn = true ; \
--- pcap.ml
+++ pcap.ml
@@ -17,30 +17,46 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with RobiNet.  If not, see <http://www.gnu.org/licenses/>.
  *)
+(**
+ * This module holds all functions related to [libpcap], packet sniffing,
+ * packet injection, and pcap file reading and writing.
+ *)
 open Batteries
 open Bitstring
 open Tools
 
 let debug = false
 
-(* Libpcap wrapper *)
+(** {1 Libpcap low level wrappers} *)
 
+(** A network device opened for sniffing or injection *)
 type iface
-external openif : string -> bool -> string -> int -> iface = "wrap_pcap_make"
-(* [openif "eth0" true "port 80" 96] returns the iface representing eth0,
-   in promiscuous mode, filtering port 80 and capturing only the first 96 bytes
-   of each packets. Notice that if caplen is set to 0 then a "default" value
-   of 65535 will be chosen, which is probably not what you want. *)
-external inject : iface -> string -> unit = "wrap_pcap_inject"
-(* [inject iface packet] inject this packet into this interface *)
-external sniff : iface -> (float * string) = "wrap_pcap_read"
-(* [sniff iface] will return the next available packet, as well as
-   its capture timestamp *)
 
+(** [openif "eth0" true "port 80" 96] returns the iface representing eth0,
+ * in promiscuous mode, filtering port 80 and capturing only the first 96 bytes
+ * of each packets. Notice that if [caplen] is set to 0 then a "default" value
+ * of 65535 will be chosen, which is probably not what you want. You should set
+ * [caplen] = your {e MTU} size. *)
+external openif : string -> bool -> string -> int -> iface = "wrap_pcap_make"
+
+(** [inject iface packet] inject this packet into this interface *)
+external inject : iface -> string -> unit = "wrap_pcap_inject"
+
+(** [sniff iface] will return the next available packet, as well as its capture
+ * timestamp *)
+external sniff : iface -> (float * string) = "wrap_pcap_read"
+
+(** {1 User functions} *)
+(** {2 Packet injection} *)
+
+(** A counter for how many packets were injected successfully. *)
 let packets_injected_ok  = Metric.Atomic.make "Pcap/Packets/Injected/Ok"
+(** A counter for how many packets we failed to inject. *)
 let packets_injected_err = Metric.Atomic.make "Pcap/Packets/Injected/Err"
+(** A counter for how many bytes were injected successfully. *)
 let bytes_out            = Metric.Counter.make "Pcap/Bytes/Out" "bytes"
 
+(** [inject_pdu iface bits] inject the packet [bits] into interface [iface]. *)
 let inject_pdu iface bits =
     if debug then Printf.printf "Pcap: injecting a packet (%s)...\n%!" (string_of_bitstring bits);
     (try
@@ -51,10 +67,15 @@ let inject_pdu iface bits =
         if debug then Printf.printf "Pcap: Cannot inject a packet\n" ;
         Metric.Atomic.fire packets_injected_err)
 
-let packets_sniffed_ok = Metric.Atomic.make "Pcap/Packets/Sniffed"
-let bytes_in = Metric.Counter.make "Pcap/Bytes/In" "bytes"
+(** {2 Packet sniffing} *)
 
-(* Lwt thread that continuously sniff packets *)
+(** A counter for how many packets were sniffed. *)
+let packets_sniffed_ok = Metric.Atomic.make "Pcap/Packets/Sniffed"
+(** A counter for how many bytes were sniffed. *)
+let bytes_in           = Metric.Counter.make "Pcap/Bytes/In" "bytes"
+
+(** [sniffer iface rx] return a Lwt thread that continuously sniff packets
+ * and pass them to the [rx] function. *)
 let sniffer iface rx =
     let rec loop () =
         lwt ts, pkt = Lwt_preemptive.detach sniff iface in
@@ -65,23 +86,42 @@ let sniffer iface rx =
         loop ()
     in loop ()
 
-(* Pcap files *)
+(** {2 Pcap files} *)
 
-(* See /usr/include/pcap/bpf.h *)
-let dlt_null    = 0l (* BSD loopback encapsulation *)                                             
-let dlt_en10mb  = 1l (* Ethernet (10Mb) *)                                                        
-let dlt_en3mb   = 2l (* Experimental Ethernet (3Mb) *)                                            
-let dlt_ax25    = 3l (* Amateur Radio AX.25 *)                                                    
-let dlt_pronet  = 4l (* Proteon ProNET Token Ring *)
-let dlt_chaos   = 5l (* Chaos *)                                                                  
-let dlt_ieee802 = 6l (* 802.5 Token Ring *)
-let dlt_arcnet  = 7l (* ARCNET, with BSD-style header *)                                          
-let dlt_slip    = 8l (* Serial Line IP *)                                                         
-let dlt_ppp     = 9l (* Point-to-point Protocol *)                                                
-let dlt_fddi    = 10l (* FDDI *)
-let dlt_linux_cooked = 113l (* Linux SLL *)
+(** {e Data Link Layers} are constant values indicating what protocol and hardware technology
+ * some captured packets were taken from. We support only the two most common: [dlt_en10mb], ie
+ * usual Ethernet cables, and [dlt_linux_cooked] corresponding to a capture on the {e any}
+ * network device on Linux. *)
+(** BSD loopback encapsulation *)
+let dlt_null    = 0l
+(** Ethernet (10Mb) *)
+let dlt_en10mb  = 1l
+(** Experimental Ethernet (3Mb) *)
+let dlt_en3mb   = 2l
+(** Amateur Radio AX.25 *)
+let dlt_ax25    = 3l
+(** Proteon ProNET Token Ring *)
+let dlt_pronet  = 4l
+(** Chaos *)
+let dlt_chaos   = 5l
+(** 802.5 Token Ring *)
+let dlt_ieee802 = 6l
+(** ARCNET, with BSD-style header *)
+let dlt_arcnet  = 7l
+(** Serial Line IP *)
+let dlt_slip    = 8l
+(** Point-to-point Protocol *)
+let dlt_ppp     = 9l
+(** FDDI *)
+let dlt_fddi    = 10l
+(** Linux SLL *)
+let dlt_linux_cooked = 113l
 
-let save ?(caplen=65535) ?(linktype=dlt_en10mb) fname =
+(** [save "file.pcap"] returns a function that will save passed packets (as [bitstring]s)
+ * in ["file.pcap"] file.
+ * @param caplen can be used to cap saved packet to a given number of bytes
+ * @param dlt can be used to change the file's DLT (you probably do not want to do that) *)
+let save ?(caplen=65535) ?(dlt=dlt_en10mb) fname =
     let out_chan = open_out_bin fname
     and file_hdr = (BITSTRING {
         0xa1b2c3d4l : 32 : littleendian ;
@@ -90,7 +130,7 @@ let save ?(caplen=65535) ?(linktype=dlt_en10mb) fname =
         0l (* this TZ *) : 32 : littleendian ;
         0l : 32 : littleendian ;
         Int32.of_int caplen : 32 : littleendian ;
-        linktype : 32 : littleendian })
+        dlt : 32 : littleendian })
     and write_pkt caplen bits =
         let ts       = Clock.now () in
         let sec      = Int32.of_float (ts) in
@@ -110,16 +150,20 @@ let save ?(caplen=65535) ?(linktype=dlt_en10mb) fname =
     Gc.finalise (fun _ -> close_out out_chan) f ;
     f
 
+(** The global header of a pcap file. *)
 type global_header = { endianness    : endian ;
                        version_major : int ;
                        version_minor : int ;
                        this_zone     : int32 ;
                        sigfigs       : int32 ;
-                       snaplen       : int32 ;
+                       snaplen       : int32 ; (** Indicate that no caplen will be smaller. We don't use this. *)
                        dlt           : int32 }
 
+(** When trying to read packets from a file that doesn't look like a pcap file. *)
 exception Not_a_pcap_file
 
+(** [read_global_header ic] reads the pcap global header from the input stream [ic]
+ * and returns a {!Pcap.global_header}. The stream should point at the file beginning. *)
 let read_global_header ic =
     let header = bitstring_of_string (IO.really_nread ic 24) ; in
     let endianness = bitmatch (takebits 32 header) with
@@ -134,8 +178,10 @@ let read_global_header ic =
           this_zone ; sigfigs ; snaplen ; dlt }
    | { _ } -> raise Not_a_pcap_file
 
-let read_next_pkt global_header in_chan =
-    let pkt_hdr = IO.really_nread in_chan 16 in
+(** [read_next_pkt global_header ic] will return the next packet that's to be read from
+ * the input stream [ic]. The {!Pcap.global_header} is used for the byte ordering. *)
+let read_next_pkt global_header ic =
+    let pkt_hdr = IO.really_nread ic 16 in
     bitmatch (bitstring_of_string pkt_hdr) with
     | { sec      : 32 : endian (global_header.endianness) ;
         usec     : 32 : endian (global_header.endianness) ;
@@ -145,7 +191,7 @@ let read_next_pkt global_header in_chan =
             (* We don't really care but the user might *)
             Printf.printf "caplen > snaplen!\n%!"
         ) ;
-        let pkt = IO.really_nread in_chan (Int32.to_int caplen) in
+        let pkt = IO.really_nread ic (Int32.to_int caplen) in
         let bits = bitstring_of_string pkt in
         let bits = if wire_len <= caplen then bits
                    else (
@@ -155,27 +201,34 @@ let read_next_pkt global_header in_chan =
         ts, bits
     | { _ } -> should_not_happen ()
 
+(** [dlt_of "file.pcap"] will return the {!Pcap.global_header} of this pcap file. *)
 let dlt_of fname =
-    let in_chan = open_in_bin fname in
-    let global_header = read_global_header in_chan in
-    close_in in_chan ;
+    let ic = open_in_bin fname in
+    let global_header = read_global_header ic in
+    close_in ic ;
     global_header.dlt
 
-(* from a pcap file, return an enumerator of (TS * bitstring) and the global header *)
+(** from a pcap file, returns an [Enum.t] of timestamps and packets, and the
+ * {!Pcap.global_header}. *)
 let load fname =
-    let in_chan = open_in_bin fname in
-    let global_header = read_global_header in_chan in
+    let ic = open_in_bin fname in
+    let global_header = read_global_header ic in
     let rec next () =
-        try read_next_pkt global_header in_chan
+        try read_next_pkt global_header ic
         with IO.No_more_input | IO.Input_closed ->
             raise Enum.No_more_elements in
     Enum.from next, global_header
 
+(** Same than {!Pcap.load} but returns only the [Enum.t]. *)
 let enum_of fname = fst (load fname)
 
+(** Informations on a pcap file. *)
 type infos = { filename : string ; data_link_type : int32 ;
                num_packets : int ; data_size : int64 ;
                start_time : float ; stop_time : float }
+
+(** Return some informations about a pcap file (require to scan the whole file,
+ * so depending on the file size it may take some time). *)
 let infos_of filename =
     let pkts, global_header = load filename in
     let min_ts = ref Float.max_num and max_ts = ref Float.min_num
@@ -203,6 +256,8 @@ let infos_of filename =
                                            stop_time = 1332451941.92178106 })
  *)
 
+(** [merge [e1 ; e2 ; e3]] will merge the three [Enumt.t] of packets in chronological
+ * order. *)
 let rec merge = function
     | [] -> Enum.empty ()
     | a :: rest ->
@@ -216,21 +271,27 @@ let rec merge = function
                                     (enum_of "tests/someweb.pcap" |> List.of_enum)
  *)
 
+(** Small utility that truncate a pcap file to the last valid packet.
+ * Useful for those interrupted/damaged pcap files with an incomplete packet at the end,
+ * that some tools then refuse to read. *)
 let repair_file fname =
-    let in_chan = open_in_bin fname in
-    let in_chan, counter = IO.pos_in in_chan in
-    let global_header = read_global_header in_chan in
+    let ic = open_in_bin fname in
+    let ic, counter = IO.pos_in ic in
+    let global_header = read_global_header ic in
     let rec aux () =
         let ofs = counter () in
-        let cont = try ignore (read_next_pkt global_header in_chan) ; true
+        let cont = try ignore (read_next_pkt global_header ic) ; true
                    with IO.No_more_input | IO.Input_closed -> false in
         if cont then aux () else ofs in
     Unix.truncate fname (aux ())
 
+(** [play tx "file.pcap"] will read packets from ["file.pcap"] and send them to [tx]
+ * copying the pcap file frame rate. Notice that we use the internal {!Clock} for this,
+ * so it's both very accurate or not accurate at all, depending on how you look at it. *)
 let play tx fname =
+    let packets = enum_of fname in
     (* With last_packet_timestamp (or None), schedule a function using the clock to read
        the next packet from the file. *)
-    let packets = enum_of fname in
     let rec read_next_pkt last_ts =
         match Enum.get packets with
             | None -> () (* pcap file is over *)
