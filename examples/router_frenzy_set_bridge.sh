f54cea84780120b5d6b1773993f825fda386baff
Cedric Cellier <rixed@happyleptic.org>
2022-09-30 14:14:37 +0200

    router_frenzy: Use veth instead of tap devices

--- examples/router_frenzy_set_bridge.sh
+++ examples/router_frenzy_set_bridge.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-sudo modprobe dummy
-sudo ip link add br0 type dummy
-sudo ifconfig br0 192.168.0.0/24
-sudo ifconfig br0 up
-sudo ip route add 192.168.1.0/24 dev br0
-sudo ip route add 192.168.2.0/24 dev br0 via 192.168.1.0
-sudo ip route add 192.168.3.0/24 dev br0 via 192.168.1.0
+# create veth0 and veth1:
+sudo ip link add type veth
+sudo ifconfig veth0 192.168.0.42 up
+sudo ifconfig veth1 up
+
+sudo ip route add 192.168.1.0/24 dev veth0 via 192.168.0.1
+sudo ip route add 192.168.2.0/24 dev veth0 via 192.168.0.1
+sudo ip route add 192.168.3.0/24 dev veth0 via 192.168.0.1
