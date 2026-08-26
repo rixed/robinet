80cbb1a2b38036bddd4ed857951242a113395968
Cedric Cellier <rixed@happyleptic.org>
2026-08-26 11:47:29 +0200

    Simplify C function taking file descr

--- sockopt.ml
+++ sockopt.ml
@@ -1,46 +1,28 @@
 (* setsockopts missing from the stdlib: *)
 open Tools
 
-external set_ttl_ : int (* filedescr *) -> int (* TTL *) -> unit = "wrap_set_ttl"
+external set_ttl : Unix.file_descr -> int (* TTL *) -> unit = "wrap_set_ttl"
 
-let set_ttl fd ttl =
-  set_ttl_ (int_of_fd fd) ttl
-
-external set_tos_ : int (* filedescr *) -> int (* TOS *) -> unit = "wrap_set_tos"
+external set_tos : Unix.file_descr -> int (* TOS *) -> unit = "wrap_set_tos"
 
 let tos_lowcost = 0x02
 let tos_reliability = 0x04
 let tos_throughput = 0x08
 let tos_lowdelay = 0x10
 
-let set_tos fd tos =
-  set_tos_ (int_of_fd fd) tos
+external set_df : Unix.file_descr -> unit = "wrap_set_df"
 
-external set_df_ : int (* filedescr *) -> unit = "wrap_set_df"
-
-let set_df fd =
-  set_df_ (int_of_fd fd)
-
-external set_tcp_syn_count_ : int (* filedescr *) -> int -> unit =
+external set_tcp_syn_count : Unix.file_descr -> int -> unit =
   "wrap_set_tcp_syn_count"
 
-let set_tcp_syn_count fd cnt =
-  set_tcp_syn_count_ (int_of_fd fd) cnt
-
-external set_recv_errs_ : int (* filedescr *) -> bool -> unit =
+external set_recv_errs : Unix.file_descr -> bool -> unit =
   "wrap_set_recv_errs"
 
-let set_recv_errs fd flag =
-  set_recv_errs_ (int_of_fd fd) flag
-
-external get_last_icmp_err_ :
-  int (* filedescr *) ->
+(* Raises Not_found if no error has been received *)
+external get_last_icmp_err :
+  Unix.file_descr ->
   int (* ICMP err type *) * int (* ICMP err code *) * Unix.inet_addr option (* emitter *) =
   "wrap_get_last_icmp_err"
-
-(* Raises Not_found if no error has been received *)
-let get_last_icmp_err fd =
-  get_last_icmp_err_ (int_of_fd fd)
 
 external htons : int -> int = "wrap_htons"
 external ntohs : int -> int = "wrap_ntohs"
